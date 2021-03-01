54cc2d7deb72bdfa3d788ec281441ecaed6ba55c
유승오 <soyoo84@gmail.com>
2021-03-01 17:21:58 +0900

    210301 유승오

--- taxiguider/taxicall/src/main/java/taxiguider/택시호출PolicyHandler.java
+++ taxiguider/taxicall/src/main/java/taxiguider/택시호출PolicyHandler.java
@@ -58,6 +58,10 @@ public class 택시호출PolicyHandler {
 		System.out.println("##### EVT TYPE[할당취소됨]  : " + 할당취소됨.getEventType());
 		if (할당취소됨.isMe()) {
 			System.out.println("##### listener[할당취소됨]  : " + 할당취소됨.toJson());
+			택시호출Repository.findById(Long.valueOf(할당취소됨.getId())).ifPresent((택시호출) -> {
+				택시호출.set호출상태("호출취소");
+				택시호출Repository.save(할당취소됨);
+			});
 		}
 	}
 
