97e1d412a9ecc85f562d01b211f80a949b9b7ee3
elice <gitaccount@elice.com>
2021-03-02 20:57:55 +0900

    명칭변경 3차

--- taxiguider/taxicall/src/main/java/taxiguider/external/TaximanageServiceFallback.java
+++ taxiguider/taxicall/src/main/java/taxiguider/external/TaximanageServiceFallback.java
@@ -13,7 +13,7 @@ public class TaximanageServiceFallback implements TaximanageService {
 	
 	
 	@Override
-	public void TaximanageAssign(Taximanage Taximanage) {
+	public void TaximanageAssign(Taximanage taximanage) {
 		// TODO Auto-generated method stub
 		System.out.println("Circuit breaker has been opened. Fallback returned instead. " + taximanage.getId());
 	}
